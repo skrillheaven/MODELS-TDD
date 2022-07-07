7cafbf96d3f2c83afe6cbd53ae0d57f781aafe01
skrillheaven <beto.jacp2010@gmail.com>
2022-07-07 14:51:29 -0500

    Requerimiento 2 pasado

--- app/user.js
+++ app/user.js
@@ -1,16 +1,25 @@
 class User {
-    constructor(id, username, name, bio, dateCreated, lastUpdate) {
+    constructor(id, username, name, bio
+        /*, dateCreated, lastUpdate*/
+    ) {
         this.id = id
         this.username = username
         this.name = name
         this.bio = bio
-        this.dateCreated = dateCreated
-        this.lastUpdate = lastUpdate
+            /*this.dateCreated = dateCreated
+            this.lastUpdate = lastUpdate*/
     }
 }
+
 class Usuario {
-
-
+    constructor(id, username, name, bio) {
+        this.id = id
+        this.username = username
+        this.name = name
+        this.bio = bio
+        this.Created = new Date()
+        this.Update = new Date()
+    }
 }
 
 module.exports = User
--- test/user.test.js
+++ test/user.test.js
@@ -2,22 +2,26 @@ const User = require('./../app/user')
 const Usuario = require('./../app/user')
 describe("Unit test for user twitter ", () => {
     test('Requerimiento 1) Guardar la info de usuario en nuestra app', () => {
-        //invocamos el codigo a usar en la app
-        const user = new User(1, "Alberto Cruz", "skrillheaven", "Bio", "dateCreated", "lastUpdated")
-            //validamos los resuktados del codigo
-            //Comparativa que va a igualar el valor de la izquierda con la derecha
-        expect(user.id).toBe(1)
-        expect(user.username).toBe("Alberto Cruz")
-        expect(user.name).toBe("skrillheaven")
-        expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdate).toBe("lastUpdated")
-    })
-    test(' Requerimiento 2) Las propiedades `dateCreated` y `lastUpdated` deberán ser datos de tipo fecha', () => {
-        const user2 = new Usuario(2, "chino1305", "diego", "ola k ase")
-        expect(user2.id).toBe(2)
-        expect(user2.username).toBe("chino1305")
-        expect(user2.name).toBe(diego)
-        expect(user2.bio).toBe("ola k ase")
-    })
+            //invocamos el codigo a usar en la app
+            const user = new User(1, "Alberto Cruz", "skrillheaven", "Bio"
+                    /*, "dateCreated", "lastUpdated"*/
+                )
+                //validamos los resuktados del codigo
+                //Comparativa que va a igualar el valor de la izquierda con la derecha
+            expect(user.id).toBe(1)
+            expect(user.username).toBe("Alberto Cruz")
+            expect(user.name).toBe("skrillheaven")
+            expect(user.bio).toBe("Bio")
+                /*expect(user.dateCreated).toBe("dateCreated")
+                                    expect(user.lastUpdate).toBe("lastUpdated") */
+        }),
+        test(' Requerimiento 2) Las propiedades `dateCreated` y `lastUpdated` deberán ser datos de tipo fecha', () => {
+            const user2 = new Usuario(2, "chino1305", "diego", "ola k ase")
+            expect(user2.id).toBe(2)
+            expect(user2.username).toBe("chino1305")
+            expect(user2.name).toBe("diego")
+            expect(user2.bio).toBe("ola k ase")
+            expect(user2.Created).not.toBeUndefined()
+            expect(user2.Update).not.toBeUndefined()
+        })
 })
