b5182d828b42febba4bba1e25f36d9f249274b7b
skrillheaven <beto.jacp2010@gmail.com>
2022-07-07 13:01:34 -0500

    invocamos el codigo a usar en la app

---
+++ test/user.test.js
@@ -0,0 +1,6 @@
+describe("Unit test for user twitter ", () => {
+    test('Requerimiento 1) Guardar la info de usuario en nuestra app', () => {
+        //invocamos el codigo a usar en la app
+        const user = new user(1, "Alberto Cruz", "skrillheaven", "Bio ", "dateCreated", "lastUpdated")
+    })
+})
