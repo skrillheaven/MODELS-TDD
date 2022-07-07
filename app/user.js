566dbdef5afa31828047c39ffcfa5e9230f4bd5f
skrillheaven <beto.jacp2010@gmail.com>
2022-07-07 14:02:18 -0500

    Prueba PASADA REQUIRIMIENTO 1

--- app/user.js
+++ app/user.js
@@ -1,5 +1,12 @@
 class User {
-    constructor(name) { this.name = name }
+    constructor(id, username, name, bio, dateCreated, lastUpdate) {
+        this.id = id
+        this.username = username
+        this.name = name
+        this.bio = bio
+        this.dateCreated = dateCreated
+        this.lastUpdate = lastUpdate
+    }
 }
 
 module.exports = User
--- test/user.test.js
+++ test/user.test.js
@@ -2,14 +2,14 @@ const User = require('./../app/user')
 describe("Unit test for user twitter ", () => {
     test('Requerimiento 1) Guardar la info de usuario en nuestra app', () => {
         //invocamos el codigo a usar en la app
-        const user = new User(1, "Alberto Cruz", "skrillheaven", "Bio ", "dateCreated", "lastUpdated")
+        const user = new User(1, "Alberto Cruz", "skrillheaven", "Bio", "dateCreated", "lastUpdated")
             //validamos los resuktados del codigo
             //Comparativa que va a igualar el valor de la izquierda con la derecha
         expect(user.id).toBe(1)
         expect(user.username).toBe("Alberto Cruz")
         expect(user.name).toBe("skrillheaven")
-        expect(user.bio).tobe("Bio")
-        expect(user.dataCreated).toBe("dateCreated")
+        expect(user.bio).toBe("Bio")
+        expect(user.dateCreated).toBe("dateCreated")
         expect(user.lastUpdate).toBe("lastUpdated")
     })
 })
