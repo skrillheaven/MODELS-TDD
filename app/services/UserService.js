037f5ea055214ff1356eeeb69261c60188a454c1
skrillheaven <beto.jacp2010@gmail.com>
2022-07-08 17:34:53 -0500

    Requerimiento 3 pasado

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -6,6 +6,10 @@ class UserService {
     static getInfo(usuario) {
         return [usuario.id, usuario.username, usuario.name, usuario.bio]
     }
+    static updateUserName(name1, new_name) {
+        return name1.username = new_name
+    }
+
 }
 
 
