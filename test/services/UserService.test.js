0cedb0672a420baaf28ace8afd56d154a407487a
skrillheaven <beto.jacp2010@gmail.com>
2022-07-08 16:35:41 -0500

    Pruebas fallidas , codigo tronando

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -3,12 +3,19 @@ const UserService = require('../../app/services/UserService')
 
 describe("Test for UserService", () => {
 
-            test("Requerimiento 1) Created a new user using the UserService", () => {
-                    const us = UserService.create(1, "Skrillheaven", "Alberto")
-                    expect(us.id).toBe(1)
-                    expect(us.username).toBe("Skrillheaven")
-                    expect(us.name).toBe("Alberto")
-                    expect(us.bio).not.toBeUndefined()
-                }),
-                test("Requerimiento 2) get all user data in a list", () => {
-                        const usget = UserService.getInfo
+    test("Requerimiento 1) Created a new user using the UserService", () => {
+            const us = UserService.create(1, "Skrillheaven", "Alberto")
+            expect(us.id).toBe(1)
+            expect(us.username).toBe("Skrillheaven")
+            expect(us.name).toBe("Alberto")
+            expect(us.bio).not.toBeUndefined()
+        }),
+        test("Requerimiento 2) get all user data in a list", () => {
+            const us = UserService.create(1, "Skrillheaven", "Alberto")
+            const usget = UserService.getInfo(user)
+            expect(usget[0]).toBe(1)
+            expect(usget[1]).toBe("Skrillheaven")
+            expect(usget[2]).toBe("Alberto")
+            expect(usget[3]).toBe("sin bio")
+        })
+})
