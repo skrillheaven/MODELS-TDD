114472dfae04d0e84f0a07fb3b9bfe9dc3b5a4eb
skrillheaven <beto.jacp2010@gmail.com>
2022-07-08 14:38:40 -0500

    *********Sprint 2: Agregar servicios*******

--- test/user.test.js
+++ test/user.test.js
@@ -1,6 +1,8 @@
-const User = require('./../app/user')
-const Usuario = require('./../app/user')
+const User = require('./../app/models/user')
+const Usuario = require('./../app/models/user')
 describe("Unit test for user twitter ", () => {
+    /*Sprint 1: Agregar modelos*/
+
     test('Requerimiento 1) Guardar la info de usuario en nuestra app', () => {
             //invocamos el codigo a usar en la app
             const user = new User(1, "Alberto Cruz", "skrillheaven", "Bio"
@@ -39,4 +41,16 @@ describe("Unit test for user twitter ", () => {
             user.setBio = "new bio"
             expect(user.bio).toBe("new bio")
         })
+        /*Sprint 2: Agregar servicios
+        Requerimiento 1: Crear un nuevo usuario con lo ya definido en el sprint anterior, usando una nueva clase llamada `UserService`.
+        Criterios de aceptación:
+        1. Esta clase deberá tener un método `create` que se pueda usar sin instanciar (static method), y que reciba solo los parámetros del `id`, `username` y `name`. 
+        2. El valor de `bio` deberá ser por default para todos los `user` creados.
+
+        Requerimiento 2: Agregar un nuevo método estático en `UserService` llamado `getInfo` que al recibir un objeto de la clase `User`, me regrese una lista con todos los valores de los atributos de dicho objeto.
+
+        Requerimiento 3: Agregar un nuevo método estático en `UserService` llamado `updateUserUsername`, que reciba un objeto de la clase `User` y un nuevo string, que actualizará el valor de `username`. 
+
+        Requerimiento 4: Agregar un nuevo método estático en `UserService` llamado `getAllUsernames`, que recibirá una lista de objetos de la clase `User`, y regresará la lista de todos los usernames de dichos objetos. */
+
 })
