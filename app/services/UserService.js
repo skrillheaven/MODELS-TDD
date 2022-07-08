d48923d45086acb88106a10dd5939d4eeff3dd15
skrillheaven <beto.jacp2010@gmail.com>
2022-07-08 16:28:04 -0500

    Requerimiento 2 lista con getinfo

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -1,7 +1,7 @@
-const User = require('./../models/user')
+const Usuario = require('./../models/user')
 class UserService {
     static create(id, username, name) {
-        return new User(id, username, name, "sin bio")
+        return new Usuario(id, username, name, "sin bio")
     }
 }
 module.exports = UserService
--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -3,11 +3,12 @@ const UserService = require('../../app/services/UserService')
 
 describe("Test for UserService", () => {
 
-    test("Requerimiento 1) Created a new user using the UserService", () => {
-        const us = UserService.create(1, "Skrillheaven", "Alberto")
-        expect(us.username).toBe("Skrillheaven")
-        expect(us.name).toBe("Alberto")
-        expect(us.id).toBe(1)
-        expect(us.bio).not.toBeUndefined()
-    })
-})
+            test("Requerimiento 1) Created a new user using the UserService", () => {
+                    const us = UserService.create(1, "Skrillheaven", "Alberto")
+                    expect(us.id).toBe(1)
+                    expect(us.username).toBe("Skrillheaven")
+                    expect(us.name).toBe("Alberto")
+                    expect(us.bio).not.toBeUndefined()
+                }),
+                test("Requerimiento 2) get all user data in a list", () => {
+                        const usget = UserService.getInfo
